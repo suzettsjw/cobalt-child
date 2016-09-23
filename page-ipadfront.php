d76e494d6642f63428922155d11435b520bb1755
suzettsjw <sjwenger@syr.edu>
2016-09-23 09:06:33 -0400

    Update page-ipadfront.php

--- page-ipadfront.php
+++ page-ipadfront.php
@@ -8,13 +8,13 @@
     <link href="https://fonticons-free-fonticons.netdna-ssl.com/kits/8903c25b/8903c25b.css" media="all" rel="stylesheet">
   </head>
 <body style="margin:0;background-color:black;font-family:‘ITC Franklin Gothic W01’,Arial;font-weight:bold;font-size:4em;line-height:1em;text-transform:uppercase;overflow:hidden;">
-    <div style="position:absolute;z-index:999;height:90%;width:100%;"></div>
+    <div style="position:absolute;z-index:999;height:85%;width:100%;"></div>
     <div class="upper-videos" style="height:1000px;display:inline-block;margin:0;">
       <div id="player"></div>
       <script src="/wp-content/themes/cobalt-child/js/vimeowrap.js"></script>
       <script>vimeowrap('player').setup({urls: ['https://vimeo.com/182716364'],repeat:'always',autoplay:'true',width:'1800',height:'898',allowFullScreen:'false'});</script>
     </div><!-- .upper-videos -->
-    <div class="lower-buttons" style="position:absolute;left:0;bottom:0;width:100%;display:inline-block;height:180px;z-index:99;">
+    <div class="lower-buttons" style="position:absolute;left:0;bottom:0;width:100%;display:inline-block;height:100px;z-index:99;">
         <div style="background-color:#6f777d; width:25%;height:100%;display:inline-block;font-size:14px;">
           <a href="/touch-directory-people/" style="color:#fff;text-decoration:none;padding:20px 0 0 50px;line-height:2.5em;"><span class="touch touch-users" style="font-size: 1.5em;margin-right:10px;"></span> People</a></div><div style="background-color:#d44500;width:25%;height:100%;display:inline-block;"><a href="/map/" style="color:#fff;text-decoration:none;padding:20px 0 0 50px;line-height:2.5em;"><span class="touch touch-map" style="font-size: 1.5em;margin-right:10px;"></span> Map</a></div><div style="background-color:#3e3d3c;width:25%;height:100%;display:inline-block;"><a href="/touch-directory-calendar/" style="color:#fff;text-decoration:none;padding:20px 0 0 50px;line-height:2.5em;"><span class="touch touch-calendar" style="font-size: 1.5em;margin-right:10px;"></span> Events</a></div><div style="background-color:#0d1d37;width:25%;height:100%;display:inline-block;"><a href="/touch-directory-news/" style="color:#fff;text-decoration:none;padding:20px 0 0 50px;line-height:2.5em;"><span class="touch touch-rss" style="font-size:1.5em;margin-right:10px;"></span> News</a>
         </div>
